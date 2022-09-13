861aedba27bcbc4b1acf6ce57fa4e318bb704311
Mike <mxnmike@me.com>
2022-09-13 10:42:00 -0700

    Initial Commit

---
+++ src/components/Gastos.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Gastos = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Gastos
---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import NuevoPresupuesto from "./NuevoPresupuesto";
+import { useState } from "react";
+const Header = ({
+  presupuesto,
+  setPresupuesto,
+  isValidPresupuesto,
+  setIsValidPresupuesto,
+}) => {
+  return (
+    <header>
+      <h1>Planificador de Gastos</h1>
+      {isValidPresupuesto ? (
+        <p>Control Presupuesto</p>
+      ) : (
+        <NuevoPresupuesto
+          presupuesto={presupuesto}
+          setPresupuesto={setPresupuesto}
+          setIsValidPresupuesto={setIsValidPresupuesto}
+        />
+      )}
+    </header>
+  );
+};
+
+export default Header;
